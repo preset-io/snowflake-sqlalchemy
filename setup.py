c6b0e3ebe43c7a51b770fe52f35fe031abf9927d
Steven Uray <stevenuray@users.noreply.github.com>
2022-05-22 19:02:32 -0700

    Update to loosened-dependencies

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
 
     install_requires=[
         'sqlalchemy<2.0.0',
-        'snowflake-connector-python@git+https://github.com/preset-io/snowflake-connector-python@1.9.0-loosened-dependencies' ,
+        'snowflake-connector-python@git+https://github.com/preset-io/snowflake-connector-python/src/snowflake/connector@v1.9.0-loosened-dependences-with-setup-fix' ,
     ],
     namespace_packages=[
         'snowflake'
